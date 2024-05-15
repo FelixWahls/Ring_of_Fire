50e483493bd81be35898093d07818cde2c3b00a0
Felix Wahls <116013891+FelixWahls@users.noreply.github.com>
2024-05-15 11:27:26 +0200

    added plus icon and material design

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,28 +1,54 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-game',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './game.component.html',
-  styleUrl: './game.component.scss',
+    selector: 'app-game',
+    standalone: true,
+    imports: [CommonModule, PlayerComponent, MatButtonModule, MatIconModule],
+    templateUrl: './game.component.html',
+    styleUrl: './game.component.scss',
 })
 export class GameComponent {
-  pickCardAnimation = false;
-  game!: Game;
+    pickCardAnimation = false;
+    currentCard: string | undefined = '';
+    game!: Game;
 
-  constructor() {
-    this.newGame();
-  }
+    constructor(public dialog: MatDialog) {
+        this.newGame();
+    }
 
-  newGame() {
-    this.game = new Game();
-    console.log(this.game);
-  }
+    newGame() {
+        this.game = new Game();
+    }
 
-  takeCard() {
-    this.pickCardAnimation = true;
-  }
+    takeCard() {
+        if (!this.pickCardAnimation) {
+            this.currentCard = this.game.stack.pop();
+            this.pickCardAnimation = true;
+            console.log('new Card: ' + this.currentCard);
+            console.log(this.game);
+            setTimeout(() => {
+                if (this.currentCard) {
+                    this.game.playedCards.push(this.currentCard);
+                }
+                this.pickCardAnimation = false;
+            }, 1000);
+        }
+    }
+
+    openDialog(): void {
+        const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+            data: { name: this.name, animal: this.animal },
+        });
+
+        dialogRef.afterClosed().subscribe((result) => {
+            console.log('The dialog was closed');
+            this.animal = result;
+        });
+    }
 }
--- src/models/game.ts
+++ src/models/game.ts
@@ -1,20 +1,20 @@
 export class Game {
-  public players: string[] = [];
-  public stack: string[] = [];
-  public playedCards: string[] = [];
-  public currentPlayer: number = 0;
+    public players: string[] = [];
+    public stack: string[] = [];
+    public playedCards: string[] = [];
+    public currentPlayer: number = 0;
 
-  constructor() {
-    for (let i = 1; i < 14; i++) {
-      this.stack.push('ace_' + i);
-      this.stack.push('clubs_' + i);
-      this.stack.push('hearts_' + i);
-      this.stack.push('diamonds_' + i);
+    constructor() {
+        for (let i = 1; i < 14; i++) {
+            this.stack.push('ace_' + i);
+            this.stack.push('clubs_' + i);
+            this.stack.push('hearts_' + i);
+            this.stack.push('diamonds_' + i);
+        }
+        shuffle(this.stack);
     }
-    shuffle(this.stack);
-  }
 }
 
 function shuffle(array: string[]) {
-  array.sort(() => Math.random() - 0.5);
+    array.sort(() => Math.random() - 0.5);
 }
